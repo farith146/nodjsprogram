f9726a6d80a9c9244eb4e73f1e9e1a38c531164d
farith146 <a.farith04@gmail.com>
2025-08-13 12:58:34 +0530

    changing port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = 80; // change from 3000 to 80
+const port = 80; // Change from 3000 to 80
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});

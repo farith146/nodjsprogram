efaf26a77f72c592347f5af50e9f2d6f7043c3a8
farith146 <a.farith04@gmail.com>
2025-08-13 09:33:44 +0530

    helloworld

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello World from Node.js on AWS!</h1>');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
